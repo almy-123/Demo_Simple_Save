749ca6398674b10720fac91f6d56740eb47965d5
19037610 <19037610@myrp.edu.sg>
2020-07-13 09:58:19 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19037610/demosimplesave/MainActivity.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id19037610.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//        String msg = "No greetings!";
+
+        //Step 2a: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Step 2b: Retrieve the saved data with the key "greeting" from the SharedPreference object
+        String msg = prefs.getString("greetings", "No greetings!");
+
+        Toast toast = Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Step 1a: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Step 1b: Obtain an instance of the SharedPreference Editor for update later
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        //Step 1c: Add the key-value pair
+        prefEdit.putString("greetings", "Hello!");
+
+        //Step 1d: Call commit() to save the changes into SharedPreferences
+        prefEdit.commit();
+    }
+}
